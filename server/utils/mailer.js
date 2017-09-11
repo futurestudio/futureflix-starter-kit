00472c76358d7edcb67843bbace20429e42ba936
Marcus Poehls <marcus.poehls@gmail.com>
2017-09-11 06:19:55 +0200

    use native Nodemailer promise

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -8,7 +8,6 @@ const PostmarkTransport = require('nodemailer-postmark-transport')
 const htmlToText = require('html-to-text')
 const Templates = Path.resolve(__dirname, '..', 'server', 'email-templates')
 const Handlebars = require('handlebars')
-const Promisify = require('es6-promisify')
 const Transporter = Nodemailer.createTransport(PostmarkTransport({
   auth: {
     apiKey: process.env.POSTMARK_API_KEY
@@ -47,15 +46,14 @@ const prepareTemplate = (filename, options = {}) => {
 
 exports.send = (template, user, subject, data) => {
   return prepareTemplate(template, data).then(({ html, text }) => {
-    return Promise.resolve({
+    const mailOptions = {
       from: `Your Name <handle@tld.io>`,
       to: user.email,
       subject: subject,
       html,
       text
-    })
-  }).then(mailOptions => {
-    const send = Promisify(Transporter.sendMail, Transporter)
-    return send(mailOptions)
+    }
+
+    return Transporter.sendMail(mailOptions)
   })
 }
