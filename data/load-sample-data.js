632cddf31d779fac5ab5a24c108283232ab4e088
Marcus Poehls <marcus.poehls@gmail.com>
2017-09-19 11:10:32 +0200

    prepare script to load sample data

--- data/load-sample-data.js
+++ data/load-sample-data.js
@@ -1,12 +1,25 @@
 'use strict'
 
+// import environment variables from local secrets.env file
 const Fs = require('fs')
 const Path = require('path')
+const Dotenv = require('dotenv')
+Dotenv.config({ path: Path.resolve(__dirname, '..', 'secrets.env') })
 
-function pumpDB () {
-  // TODO
+const Models = require(Path.resolve(__dirname, '..', 'server', 'models'))
+const Movie = Models.Movie
+const Show = Models.Show
+
+function pumpItUp() {
+  console.log('Importing data 📺 👌')
 }
 
-function destroyDB () {
-  // TODO
+function destroyDB() {
+  console.log('Au revior data 😢 🔥')
+}
+
+if (process.argv.includes('--destroy')) {
+  destroyDB()
+} else {
+  pumpItUp()
 }
