684440140e8af60a2c94af1b99e8d101e0507f66
Marcus Poehls <marcus.poehls@gmail.com>
2017-12-29 14:31:53 +0100

    extend paginator to generate HAL-like links: first, prev, next, last; update pagination partial

--- server/utils/paginator.js
+++ server/utils/paginator.js
@@ -18,18 +18,24 @@ class Paginator {
     const currentPage = this.getCurrentPage(request)
     const from = currentPage * perPage - perPage
 
-    const previous = this.getPrevious(request, currentPage)
+    const first = this.getFirst(request, currentPage)
+    const last = this.getLast(request, lastPage)
+    const prev = this.getPrevious(request, currentPage)
     const next = this.getNext(request, currentPage, lastPage)
 
+    // respond a pagination JS object
     return {
       total: totalCount,
       perPage,
       currentPage,
       lastPage,
+      first,
+      prev,
       next,
-      previous,
+      last,
       from,
-      to: from + perPage
+      to: from + perPage,
+      link: this.linkHeader(first, prev, next, last)
     }
   }
 
@@ -37,22 +43,38 @@ class Paginator {
     return parseFloat(request.query.page) || 1
   }
 
+  getFirst (request, currentPage) {
+    if (!this.isFirstPage(currentPage)) {
+      return this.composeUrl(request, 1)
+    }
+  }
+
+  getLast (request, lastPage) {
+    if (!this.isLastPage(this.getCurrentPage(request), lastPage)) {
+      return this.composeUrl(request, lastPage)
+    }
+  }
+
   getNext (request, currentPage, lastPage) {
     // return if current page is the last one, there's no more
-    if (currentPage === lastPage) {
-      return
+    if (!this.isLastPage(currentPage, lastPage)) {
+      return this.composeUrl(request, currentPage + 1)
     }
-
-    return this.composeUrl(request, currentPage + 1)
   }
 
   getPrevious (request, currentPage) {
     // return if current page is the first one, there's no zero :)
-    if (currentPage === 1) {
-      return
+    if (!this.isFirstPage(currentPage)) {
+      return this.composeUrl(request, currentPage - 1)
     }
+  }
 
-    return this.composeUrl(request, currentPage - 1)
+  isFirstPage (currentPage) {
+    return currentPage === 1
+  }
+
+  isLastPage (currentPage, lastPage) {
+    return currentPage === lastPage
   }
 
   hasPage (request) {
@@ -60,12 +82,37 @@ class Paginator {
   }
 
   composeUrl (request, page) {
+    // fetch HTTP protocol from reverse proxy
     const proxyProtocol = request.headers && request.headers['x-forwarded-proto']
+    // protocol hierarchy: proxy, server, default 'http'
     const protocol = proxyProtocol || request.server.info.protocol || 'http'
+
+    // compose URL for given page
     const queryParams = Object.assign(request.query, { page })
     const query = Querystring.stringify(queryParams)
 
     return `${protocol}://${request.info.host}${request.path}?${query}`
+  }
+
+  linkHeader (first, prev, next, last) {
+    return this.createLinkHeader({
+      first,
+      prev,
+      next,
+      last
+    })
+  }
+
+  createLinkHeader (links) {
+    const headers = []
+
+    _.forOwn(links, (link, key) => {
+      if (link) {
+        headers.push(`<${link}>; rel="${key}"`)
+      }
+    })
+
+    return headers.join(', ') || undefined
   }
 }
 
