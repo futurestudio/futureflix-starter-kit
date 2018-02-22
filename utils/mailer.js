3660763049ce7b30e3778f1f09310ced68f4f694
Marcus Poehls <marcus.poehls@gmail.com>
2018-02-22 08:58:33 +0100

    refactor mailer method to prepare the template

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,9 +3,11 @@
 const Fs = require('fs')
 const Path = require('path')
 const Boom = require('boom')
+const Util = require('util')
+const ReadFile = Util.promisify(Fs.readFile)
 const Nodemailer = require('nodemailer')
 const Handlebars = require('handlebars')
-const htmlToText = require('html-to-text')
+const HtmlToText = require('html-to-text')
 const PostmarkTransport = require('nodemailer-postmark-transport')
 const Transporter = Nodemailer.createTransport(
   PostmarkTransport({
@@ -20,30 +22,27 @@ const Templates = Path.resolve(__dirname, '..', 'email-templates')
  * filename: email template name, without ".html" file ending. Email templates are located within "server/email-templates"
  * options: data which will be used to replace the placeholders within the template
  **/
-const prepareTemplate = (filename, options = {}) => {
-  return new Promise((resolve, reject) => {
-    const filePath = Path.resolve(Templates, `${filename}.html`)
+async function prepareTemplate (filename, options = {}) {
+  try {
+    const templatePath = Path.resolve(Templates, `${filename}.html`)
+    const content = await ReadFile(templatePath, 'utf8')
 
-    Fs.readFile(filePath, 'utf8', (err, fileContent) => {
-      if (err) {
-        return reject(Boom.badImplementation('Cannot read the email template content.'))
-      }
+    // use handlebars to render the email template
+    // handlebars allows more complex templates with conditionals and nested objects, etc.
+    // this way we have much more options to customize the templates based on given data
+    const template = Handlebars.compile(content)
+    const html = template(options)
 
-      // use handlebars to render the email template
-      // handlebars allows more complex templates with conditionals and nested objects, etc.
-      // this way we have much more options to customize the templates based on given data
-      const template = Handlebars.compile(fileContent)
-      const compiledHtml = template(options)
+    // generate a plain-text version of the same email
+    const text = HtmlToText.fromString(html)
 
-      // generate a plain-text version of the same email
-      const textContent = htmlToText.fromString(compiledHtml)
-
-      return resolve({
-        html: compiledHtml,
-        text: textContent
-      })
-    })
-  })
+    return {
+      html,
+      text
+    }
+  } catch (error) {
+    throw new Boom('Cannot read the email template content.')
+  }
 }
 
 /**
